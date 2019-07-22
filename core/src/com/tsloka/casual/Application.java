d6f8017e2fe8a86ab6c8f7f500d585b77876ea09
Suok <suoku667@gmail.com>
2019-07-22 07:02:20 +0200

    update

--- core/src/com/tsloka/casual/Application.java
+++ core/src/com/tsloka/casual/Application.java
@@ -22,19 +22,66 @@ public class Application extends ApplicationAdapter {
     private final int leftEndX = -400;
     private final int leftEndY = -400;
 
-    public void createFiguresTextures() {
-        Sprite blackBishop = new Sprite(new Texture("core/assets/BlackBishop.png"));
-        Sprite blackKing = new Sprite(new Texture("core/assets/BlackKing.png"));
-        Sprite blackKnight = new Sprite(new Texture("core/assets/BlackKnight.png"));
-        Sprite blackPawn = new Sprite(new Texture("core/assets/BlackPawn.png"));
-        Sprite blackQueen = new Sprite(new Texture("core/assets/BlackQueen.png"));
-        Sprite blackRook = new Sprite(new Texture("core/assets/BlackRook.png"));
-        Sprite whiteBishop = new Sprite(new Texture("core/assets/WhiteBishop.png"));
-        Sprite whiteKing = new Sprite(new Texture("core/assets/WhiteKing.png"));
-        Sprite whiteKnight = new Sprite(new Texture("core/assets/WhiteKnight.png"));
-        Sprite whitePawn = new Sprite(new Texture("core/assets/WhitePawn.png"));
-        Sprite whiteQueen = new Sprite(new Texture("core/assets/WhiteQueen.png"));
-        Sprite whiteRook = new Sprite(new Texture("core/assets/WhiteRook.png"));
+    private static Texture blackBishop;
+    private static Texture blackKing;
+    private static Texture blackKnight;
+    private static Texture blackPawn;
+    private static Texture blackQueen;
+    private static Texture blackRook;
+    private static Texture whiteBishop;
+    private static Texture whiteKing;
+    private static Texture whiteKnight;
+    private static Texture whitePawn;
+    private static Texture whiteQueen;
+    private static Texture whiteRook;
+
+    public Texture selectFigureTexture(FigureType figureType, Colour colour) {
+        Texture returnTexture = null;
+        switch (figureType) {
+            case BISHOP:
+                if (colour == Colour.BLACK) {
+                    returnTexture = blackBishop;
+                } else {
+                    returnTexture = whiteBishop;
+                }
+                break;
+            case KING:
+                if (colour == Colour.BLACK) {
+                    returnTexture = blackKing;
+                } else {
+                    returnTexture = whiteKing;
+                }
+                break;
+            case KNIGHT:
+                if (colour == Colour.BLACK) {
+                    returnTexture = blackKnight;
+                } else {
+                    returnTexture = whiteKnight;
+                }
+                break;
+            case PAWN:
+                if (colour == Colour.BLACK) {
+                    returnTexture = blackPawn;
+                } else {
+                    returnTexture = whitePawn;
+                }
+                break;
+            case QUEEN:
+                if (colour == Colour.BLACK) {
+                    returnTexture = blackQueen;
+                } else {
+                    returnTexture = whiteQueen;
+                }
+                break;
+            case ROOK:
+                if (colour == Colour.BLACK) {
+                    returnTexture = blackRook;
+                } else {
+                    returnTexture = whiteRook;
+                }
+                break;
+        }
+        return returnTexture;
     }
 
     @Override
@@ -43,7 +90,18 @@ public class Application extends ApplicationAdapter {
         shapeRenderer = new ShapeRenderer();
         camera = new OrthographicCamera(resolutionX, resolutionY);
         backgroundSprite = new Sprite(new Texture("core/assets/indigo-leather.png"));
-        createFiguresTextures();
+        blackBishop = new Texture("core/assets/BlackBishop.png");
+        blackKing = new Texture("core/assets/BlackKing.png");
+        blackKnight = new Texture("core/assets/BlackKnight.png");
+        blackPawn = new Texture("core/assets/BlackPawn.png");
+        blackQueen = new Texture("core/assets/BlackQueen.png");
+        blackRook = new Texture("core/assets/BlackRook.png");
+        whiteBishop = new Texture("core/assets/WhiteBishop.png");
+        whiteKing = new Texture("core/assets/WhiteKing.png");
+        whiteKnight = new Texture("core/assets/WhiteKnight.png");
+        whitePawn = new Texture("core/assets/WhitePawn.png");
+        whiteQueen = new Texture("core/assets/WhiteQueen.png");
+        whiteRook = new Texture("core/assets/WhiteRook.png");
     }
 
     @Override
@@ -54,16 +112,10 @@ public class Application extends ApplicationAdapter {
         backgroundSprite.draw(spriteBatch);
         camera.update();
         spriteBatch.end();
-        shapeRenderer.setProjectionMatrix(camera.combined);
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+
         for (Field field : board.getFields()) {
-            if (!field.isEmpty()) {
-                Sprite figureSprite = new Sprite();
-                        leftEndX + 5 + (field.getColumn() * 100),
-                        leftEndY + 5 + (field.getRow() * 100),
-                        90, 90);
-                figureSprite.draw(spriteBatch);
-            }
+            shapeRenderer.setProjectionMatrix(camera.combined);
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
             if (field.getColour() == Colour.WHITE) {
                 shapeRenderer.setColor(Color.LIGHT_GRAY);
             } else {
@@ -72,14 +124,27 @@ public class Application extends ApplicationAdapter {
             shapeRenderer.rect(leftEndX + (field.getColumn() * 100),
                     leftEndY + (field.getRow() * 100),
                     100, 100);
+            shapeRenderer.end();
+
+            //This part either doesn`t work or creates figures in lower left corner
+            if (!field.isEmpty()) {
+                spriteBatch.begin();
+                Sprite figureSprite = new Sprite((selectFigureTexture(field.getFigure().getType(), field.getFigure().getColour())),
+                        leftEndX + 5 + (field.getColumn() * 100),
+                        leftEndY + 5 + (field.getRow() * 100),
+                        90, 90);
+                figureSprite.draw(spriteBatch);
+                spriteBatch.end();
+            }
+
         }
 
-        shapeRenderer.end();
+
     }
 
     @Override
     public void dispose() {
         spriteBatch.dispose();
-        backgroundTexture.dispose();
+        shapeRenderer.dispose();
     }
 }
